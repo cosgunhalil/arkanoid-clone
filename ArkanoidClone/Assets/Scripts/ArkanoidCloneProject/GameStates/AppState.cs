fc60feddb6dde2ce259fbf7b86dc459be046758b
Halil Coşgun <cosgun.halil@gmail.com>
2021-10-28 22:38:55 +0300

    feat(base): Add game states
    in game state,
    end game state,
    pause game state,
    main menu state,
    app state

---
+++ ArkanoidClone/Assets/Scripts/ArkanoidCloneProject/GameStates/AppState.cs
@@ -0,0 +1,18 @@
+namespace ArkanoidProject.State 
+{
+    using Devkit.HSM;
+
+    public class AppState : StateMachine
+    {
+        protected override void OnEnter()
+        {
+            //TODO: handle
+        }
+
+        protected override void OnExit()
+        {
+            //TODO: handle
+        }
+    }
+}
+
---
+++ ArkanoidClone/Assets/Scripts/ArkanoidCloneProject/GameStates/InGameState.cs
@@ -0,0 +1,23 @@
+namespace ArkanoidProject.State 
+{
+    using Devkit.HSM;
+
+    public class InGameState : StateMachine
+    {
+        protected override void OnEnter()
+        {
+            //TODO: handle
+        }
+
+        protected override void OnExit()
+        {
+            //TODO: handle
+        }
+
+        protected override void OnUpdate()
+        {
+            //TODO: handle
+        }
+    }
+}
+
