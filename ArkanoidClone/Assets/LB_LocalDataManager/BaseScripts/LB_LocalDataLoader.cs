955f76346649f62f7d4576759f4b2c5752da73ca
Halil Coşgun <cosgun.halil@gmail.com>
2023-04-03 23:50:20 +0300

    feat(data): Add data loader / saver

---
+++ ArkanoidClone/Assets/LB_LocalDataManager/BaseScripts/LB_LocalDataLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LB_LocalDataLoader : MonoBehaviour
+{
+    public T LoadData<T>(string fileName)
+    {
+#if UNITY_EDITOR
+        string path = Application.dataPath + "/" + fileName + ".txt";
+#else
+        string path = Application.persistentDataPath + "/" + fileName + ".txt";
+#endif
+        var data = ReadDataFromPath(path);
+        return JsonUtility.FromJson<T>(data);
+    }
+
+    public string ReadDataFromPath(string path)
+    {
+        string data = null;
+        try
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    data = reader.ReadToEnd();
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+            //TODO: handle exception
+        }
+
+        return data;
+    }
+
+}
